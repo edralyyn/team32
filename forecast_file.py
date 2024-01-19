6d1701304ec179ef6439643704f79eacbb33dc1a
edralyyn <qerperalta@tip.edu.ph>
2024-01-20 02:58:16 +0800

    new

---
+++ forecast_file.py
@@ -0,0 +1,7 @@
+# forecast_file.py
+
+def run_forecast():
+    print("Code from forecast_file.py is executed!")
+
+if __name__ == "__main__":
+    run_forecast()
---
+++ other_file.py
@@ -0,0 +1,7 @@
+# other_file.py
+
+def main():
+    print("Hello from other_file.py!")
+
+if __name__ == "__main__":
+    main()
