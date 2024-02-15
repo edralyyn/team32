15f9de430243efa2a3f1f1a84592387d3c9bc288
edralyyn <qerperalta@tip.edu.ph>
2024-02-16 02:47:08 +0800

    icon

--- MAIN/main.py
+++ MAIN/main.py
@@ -6,6 +6,32 @@ import os
 import predict
 import tkinter.simpledialog as simpledialog
 
+def set_icon(window):
+    icon_filename = "icon.ico"
+    icon_path = os.path.join(os.getcwd(), icon_filename)
+    window.iconbitmap(icon_path)
+
+def custom_askinteger(title, prompt):
+    while True:
+        dialog_window = tk.Toplevel()
+        set_icon(dialog_window)
+        dialog_window.title(title)
+        label = tk.Label(dialog_window, text=prompt)
+        label.pack(padx=10, pady=5)
+        entry_var = tk.StringVar()
+        entry = tk.Entry(dialog_window, textvariable=entry_var)
+        entry.pack(padx=10, pady=5)
+        ok_button = tk.Button(dialog_window, text="Predict", command=lambda: dialog_window.destroy())
+        ok_button.pack(pady=5)
+        dialog_window.wait_window()
+        result = None
+        try:
+            result = int(entry_var.get())
+            break  # Break the loop if a valid integer is entered
+        except ValueError:
+            messagebox.showerror("Invalid Input", "Please enter a valid integer.")
+    return result
+
 def on_collect_click(information_text):
     result = messagebox.askyesno("Warning", "This will collect data. Do you want to proceed?")
     if result:
@@ -19,7 +45,7 @@ def on_forecast_click():
     result = messagebox.askyesno("Warning", "This will run forecasting. Do you want to proceed?")
     if result:
         try:
-            num_days_input = simpledialog.askinteger("Input", "Enter the number of days for prediction:")
+            num_days_input = custom_askinteger("Desired Days", "Enter the number of days for prediction:")
             for file_name, df in predict.dataframes:
                 predicted_pid = predict.predict_pid(df, num_days_input)
                 predict.predictions[file_name] = predicted_pid
@@ -30,6 +56,7 @@ def on_forecast_click():
 
 def show_forecasting_information(predictions, error_output, num_days_input):
     forecasting_window = tk.Toplevel()
+    set_icon(forecasting_window)
     forecasting_window.title("Forecasting Information")
     forecasting_window.configure(bg='#238BD6')
     forecasting_label = tk.Label(forecasting_window, text="Forecasting information:", bg='#238BD6', fg='black')
@@ -44,8 +71,8 @@ def show_forecasting_information(predictions, error_output, num_days_input):
 
 def customize_gui(background_color, window_size):
     root = tk.Tk()
+    set_icon(root)
     root.title("Predictive Maintenance System")
-
     root.configure(bg=background_color)
     root.geometry(window_size)
 
